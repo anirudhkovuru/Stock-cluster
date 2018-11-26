55912dffd076b100cd483b1597c4d66fdc07b450
ChanakyaVishal <chanakyavishal.k@students.iiit.ac.in>
2018-11-27 00:43:43 +0530

    Stock data visualization

--- visualize.py
+++ visualize.py
@@ -18,22 +18,21 @@ def graph_HL_stock(stock_list):
 
 def graph_mean_open_iteration(val):
     color_pointer = 1
-    data = pd.read_csv()
     cluster_means = []
-    with (open('output/chckpt_' + val, "rb")) as openfile:
-        while True:
-            try:
-                cluster_means.append(pickle.load(openfile))
-            except EOFError:
-                break
-    data = pd.DataFrame(cluster_means, columns=["OC", "Volume", "High", "Close"])
-
+    file_name = 'D:\Sem V\DWDM\DWDM Final\Stock-cluster\output\chckpt_' + str(val)
+    print(file_name)
+    file = (open(file_name, "rb"))
+    cluster_means.append(pickle.load(file))
+    print(cluster_means[0][1])
+    data = pd.DataFrame(cluster_means[0][0], columns=["OC", "Volume", "High", "Close"])
+    data2 = pd.DataFrame(cluster_means[0][2], columns=["OC", "Volume", "High", "Close"])
+    print(data)
+    print(data2)
     plt.plot(data['OC'], color=color_list[color_pointer])
+    plt.plot(data2['OC'], color=color_list[color_pointer + 2])
 
-    color_pointer += 1
-    color_pointer = color_pointer % 6
     plt.show()
 
 
 if __name__ == '__main__':
-        graph_HL_stock(["SIG", "MAA"])
+    graph_mean_open_iteration(20)
