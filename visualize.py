1b607170f9ee21a9c451ffcc20ffd97f371e22da
ChanakyaVishal <chanakyavishal.k@students.iiit.ac.in>
2018-11-26 18:07:54 +0530

    Basic visualization

---
+++ visualize.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+
+color_list = {1:'b', 2:'g', 3:'r', 4:'c',5:'m', 6:'y', 7:'k', 8:'w'}
+
+def graph_HL_stock(stock_list):
+    color_pointer = 1
+    for stock in stock_list:
+        data = pd.read_csv('preprocessed_stocks/' + stock + ".csv")
+
+        plt.plot(data['date'], data['high'] - data['low'], color=color_list[color_pointer])
+
+
+        color_pointer += 1
+        color_pointer = color_pointer % 6
+    plt.show()
+
+def graph_mean_open_iteration(val):
+    color_pointer = 1
+    data = pd.read_csv()
+    cluster_means = []
+    with (open('output/chckpt_' + val, "rb")) as openfile:
+        while True:
+            try:
+                cluster_means.append(pickle.load(openfile))
+            except EOFError:
+                break
+    data = pd.DataFrame(cluster_means, columns=["OC", "Volume", "High", "Close"])
+
+    plt.plot(data['OC'], color=color_list[color_pointer])
+
+    color_pointer += 1
+    color_pointer = color_pointer % 6
+    plt.show()
+
+
+if __name__ == '__main__':
+        graph_HL_stock(["SIG", "MAA"])
